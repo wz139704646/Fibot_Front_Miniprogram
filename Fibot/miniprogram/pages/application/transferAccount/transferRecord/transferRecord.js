69647db60817479c47d1c27863a31b56c8bf08ef
“Byron” <1085436942@qq.com>
2019-08-04 12:21:22 +0800

    0804-zqr add two pages about fund transfer

---
+++ Fibot/miniprogram/pages/application/transferAccount/transferRecord/transferRecord.js
@@ -0,0 +1,25 @@
+const app = getApp()
+const host = app.globalData.requestHost
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  addTransfer(e){
+    wx.navigateTo({
+      url: '../newTransfer/newTransfer',
+    })
+  }
+})
