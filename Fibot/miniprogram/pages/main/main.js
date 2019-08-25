01f97aaf9a929f4ca34997b3829fa66ae9bd7be0
fengshiwest <42319918+fengshiwest@users.noreply.github.com>
2019-08-25 21:28:44 +0800

    0825-wy-AddorDeleteApplication

--- Fibot/miniprogram/pages/main/main.js
+++ Fibot/miniprogram/pages/main/main.js
@@ -45,12 +45,6 @@ Page({
         badge: 0,
         name: '客户列表',
         url: "/pages/application/customerList/customerList"
-      }, {
-        icon: 'cart',
-        color: 'olive',
-        badge: 0,
-        name: '销售开单',
-        url: "/pages/application/sellBill/sellBill"
       }
     ],
 
@@ -102,6 +96,63 @@ Page({
     console.log("navigate")
     wx.navigateTo({
       url: e.currentTarget.id,
+    })
+  },
+
+  //长按删除功能
+  delete: function (e) {
+    var that = this;
+    var list = that.data.sellIconList;
+    var index = e.currentTarget.dataset.index;//获取当前长按下标
+    wx.showModal({
+      title: '提示',
+      content: '确定要删除该功能吗？',
+      success: function (res) {
+        if (res.confirm) {
+          list.splice(index, 1);
+        } else if (res.cancel) {
+          return false;
+        }
+        that.setData({
+          sellIconList: list
+        });
+      }
+    })
+  },
+
+  //添加功能
+  add: function (e) {
+    var that = this;
+    var list = that.data.sellIconList;
+    var index = e.currentTarget.dataset.index;//获取当前长按下标
+    wx.showModal({
+      title: '提示',
+      content: '确定要添加该功能吗？',
+      success: function (res) {
+        if (res.confirm) {
+          list.push(list[index]);
+        } else if (res.cancel) {
+          return false;
+        }
+        that.setData({
+          sellIconList: list,
+          isShow: false
+        });
+      }
+    })
+  },
+
+  //点击添加
+  show: function () {
+    this.setData({
+      isShow: true
+    })
+  },
+
+  //取消添加
+  hide: function () {
+    this.setData({
+      isShow: false
     })
   }
 
