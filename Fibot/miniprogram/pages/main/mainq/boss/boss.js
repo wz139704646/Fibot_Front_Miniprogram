0a5de47991160c485944acf423bb9e828ccc052d
fengshiwest <42319918+fengshiwest@users.noreply.github.com>
2019-09-10 22:18:34 +0800

    0910-wy-createbugs

--- Fibot/miniprogram/pages/main/mainq/boss/boss.js
+++ Fibot/miniprogram/pages/main/mainq/boss/boss.js
@@ -1,66 +1,287 @@
 // pages/main/boss/boss.js
+const app = getApp();
+var wxCharts = require('../../../../utils/wxcharts-min.js');
+const host = app.globalData.requestHost
+const applicationBase = app.globalData.applicationBase
+var pieChart1 = null;
+var pieChart2 = null;
+var arr = null;
+var startPos = null;
+var windowWidth = wx.getSystemInfoSync().windowWidth - 15
+var firstOpen = true
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    distance: 10000,
+    activeNames: [],
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    //展示在页面图标
+    showIconList: [{
+      icon: 'ticket',
+      color: 'red',
+      badge: 0,
+      name: '凭证',
+      url: applicationBase + '/pages/voucher/voucherList/voucher'
+    }, {
+      icon: 'copy',
+      color: 'yellow',
+      badge: 0,
+      name: '报表',
+      url: ''
+    }
+    ],
+    //可添加图标
+    addIconList: [{
+      icon: 'sort',
+      color: 'yellow',
+      badge: 0,
+      name: '科目余额',
+      url: applicationBase + '/pages/trialBalance/trialBalance'
+    },
+    {
+      icon: 'text',
+      color: 'blue',
+      badge: 0,
+      name: '科目表',
+      url: applicationBase + '/pages/subjects/subjects/subjects'
+    },
+    {
+      icon: 'rankfill',
+      color: 'orange',
+      badge: 0,
+      name: '存款日记账',
+      url: applicationBase + '/pages/depositJournal/addJournal/addJournal'
+    }, {
+      icon: 'refund',
+      color: 'red',
+      badge: 0,
+      name: '库存现金',
+      url: applicationBase + '/pages/cashOnHand/cashShow/cashShow'
+    },
+    {
+      icon: 'copy',
+      color: 'green',
+      badge: 0,
+      name: '银行对账',
+      url: applicationBase + '/pages/bankReconciliations/reconciliationShow/reconciliationShow'
+    },
+    {
+      icon: 'text',
+      color: 'yellow',
+      badge: 0,
+      name: '资金日报表',
+      url: applicationBase + '/pages/dailyFund/dailyFund'
+    }
+    ],
 
+    statis: [{
+      title: '营收分析',
+      showMonth: false,
+      showPeriod: true,
+      showIdx: 0
+    },
+      // {
+      //   title: '资产增长分析',
+      //   showMonth: true,
+      //   showPeriod: false
+      // }
+    ],
+
+    chartHidden: true,
+    CustomBar: app.globalData.CustomBar,
+
+    liftimes: {
+      attached: function () {
+        console.log('高层管理')
+        this.drawDiagram()
+      }
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.setData({
+      showIconList: this.data.showIconList
+    })
+    console.log('高层管理')
+    this.drawDiagram()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-    console.log('财务人员')
+    console.log('高层管理')
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  NavToTalk(e) {
+    wx.navigateTo({
+      url: applicationBase + '/pages/start/start',
+    })
+    console.log("navigate")
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  //分页面跳转
+  show(e) {
+    console.log("navigate")
+    wx.navigateTo({
+      url: e.currentTarget.id,
+    })
+    console.log('高层管理')
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  //长按删除功能
+  delete: function (e) {
+    var that = this;
+    var list = that.data.showIconList;
+    var index = e.currentTarget.dataset.index;//获取当前长按下标
+    wx.showModal({
+      title: '提示',
+      content: '确定要删除该功能吗？',
+      success: function (res) {
+        if (res.confirm) {
+          list.splice(index, 1);
+        } else if (res.cancel) {
+          return false;
+        }
+        that.setData({
+          showIconList: list
+        });
+      }
+    })
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  onChange(event) {
+    this.setData({
+      activeNames: event.detail,
+      showIconList: this.data.showIconList
+    });
+    console.log('高层管理')
+    this.drawDiagram()
+    if (firstOpen) {
+      wx.navigateTo({
+        url: '/pages/main/mainq/boss/boss',
+        success: function () {
+          console.log('nav')
+        }
+      })
+      firstOpen = false
+    }
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  clickbtn: function (e) {
+    console.log('click')
+    this.drawDiagram()
+    this.setData({
+      distance: 200,
+      chartHidden: false
+    })
+  },
 
-  }
+  //添加功能
+  add: function (e) {
+    var that = this;
+    var list = that.data.addIconList;
+    var index = e.currentTarget.dataset.index;//获取当前长按下标
+    wx.showModal({
+      title: '提示',
+      content: '确定要添加该功能吗？',
+      success: function (res) {
+        if (res.confirm) {
+          showIconList.push(list[index]);
+        } else if (res.cancel) {
+          return false;
+        }
+        that.setData({
+          showIconList: showIconList,
+          isShow: false
+        });
+      }
+    })
+  },
+
+  //点击添加
+  showFunction: function () {
+    this.setData({
+      isShow: true
+    })
+  },
+
+  //取消添加
+  hide: function () {
+    this.setData({
+      isShow: false
+    })
+  },
+
+  //点击图片触发
+  touchHandler: function (e) {
+    console.log('touch')
+    wx.showModal({
+      content: arr[pieChart1.getCurrentDataIndex(e)].name + '营业收入' + arr[pieChart1.getCurrentDataIndex(e)].data + '元',
+      showCancel: false,
+      confirmText: "我知道啦",
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        }
+      }
+    });
+  },
+
+  touchEndHandler: function (e) {
+    console.log('touch')
+    if (this.data.statis.title == '营收分析') {
+      var intro_text = '总营业收入为'
+    }
+    else if (this.data.statis.title == '营业支出分析') {
+      var intro_text = '总营业支出为'
+    }
+    wx.showModal({
+      content: arr[pieChart1.getCurrentDataIndex(e)].name + '营业收入' + arr[pieChart1.getCurrentDataIndex(e)].data + '元',
+      showCancel: false,
+      confirmText: "我知道啦",
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        }
+      }
+    });
+  },
+
+  //画图函数
+  drawDiagram: function (year = 0, month = 0) {
+    console.log("draw")
+    var token = app.getToken()
+    wx.request({
+      url: host + '/data/getTotalOperatingIncome',
+      method: "POST",
+      header: {
+        'Content-Type': 'application/json',
+        'Authorization': token
+      },
+      success: res => {
+        arr = []
+        for (var k in res.data.result) {
+          arr.push({
+            name: k,
+            data: res.data.result[k]
+          });
+          console.log(arr)
+        }
+        pieChart1 = new wxCharts({
+          animation: true,
+          canvasId: 'pieCanvas1',
+          type: 'pie',
+          series: arr,
+          width: windowWidth,
+          height: 300,
+          dataLabel: true,
+        });
+      },
+      fail: res => {
+        console.error("未成功获取到营业支出数据")
+      },
+      complete: res => {
+
+      }
+    })
+  },
 })
