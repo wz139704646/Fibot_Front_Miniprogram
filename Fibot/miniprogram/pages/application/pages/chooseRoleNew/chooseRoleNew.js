50fb353fec9276997825f485272029f0b32f4294
“Byron” <1085436942@qq.com>
2019-11-17 18:17:31 +0800

    1117-fix role chosen

--- Fibot/miniprogram/pages/application/pages/chooseRoleNew/chooseRoleNew.js
+++ Fibot/miniprogram/pages/application/pages/chooseRoleNew/chooseRoleNew.js
@@ -1,57 +1,51 @@
 // pages/application/chooseRoleNew/chooseRoleNew.js
 const app = getApp();
 const mainBase = app.globalData.mainBase
+const cloudBase = "cloud://zqr-testdemo-974f2b.7a71-zqr-testdemo-974f2b-1258658144"
 Page({
   data: {
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
     ColorList: app.globalData.ColorList,
-  },
-  onLoad: function () { },
-  pageBack() {
-    wx.navigateBack({
-      delta: 1
-    });
-  }
-});
 
-Component({
-  options: {
-    addGlobalClass: true,
-  },
-  data: {
+    cardCur: 0,
     elements: [{
       title: '高层管理',
       name: '统筹一切 负责公司整体运作',
       color: 'cyan',
-      url: mainBase + '/mainq/high_level/high_level'
-      //icon: 'newsfill'
+      image: cloudBase + "/imgs/人物角色/高层管理.png",
+      url: mainBase + '/mainq/high_level/high_level',
+      icon: 'newsfill'
     },
     {
       title: '人资管理',
       name: '人力资源 管理公司人事调度',
       color: 'blue',
-      url: mainBase + '/mainq/HR/HR'
-      //icon: 'colorlens'
+      url: mainBase + '/mainq/HR/HR',
+      image: cloudBase + "/imgs/人物角色/人资管理.png",
+      icon: 'colorlens'
     },
     {
       title: '财会管理',
       name: '财务会计 掌管公司财务大计',
       color: 'olive',
-      url: mainBase + '/mainq/finance/finance'
-      //icon: 'font'
+      image: cloudBase + "/imgs/人物角色/财会管理.png",
+      url: mainBase + '/mainq/finance/finance',
+      icon: 'font'
     },
     {
       title: '行政管理',
       name: '行政事务 统筹公司组织管理',
       color: 'green',
-      url: mainBase + "/mainq/administration/administration"
-      //icon: 'icon'
+      image: cloudBase + "/imgs/人物角色/行政管理.png",
+      url: mainBase + "/mainq/administration/administration",
+      icon: 'icon'
     },
     {
       title: '生产管理',
       name: '基层生产 扎实公司底层实力',
       color: 'grey',
+      image: cloudBase + "/imgs/人物角色/生产管理.png",
       url: mainBase + '/mainq/purchase/purchase'
       //icon: 'btn'
     },
@@ -59,23 +53,45 @@ Component({
       title: '销售管理',
       name: '市场销售 贯通公司销售之道',
       color: 'purple',
-      url: mainBase + '/mainq/seller/seller'
-      //icon: 'tagfill'
+      image: cloudBase + "/imgs/人物角色/销售管理.png",
+      url: mainBase + '/mainq/seller/seller',
+      icon: 'tagfill'
     },
     {
       title: '市场营销',
       name: '精准营销 推广公司品牌力量',
       color: 'brown',
-      url: mainBase + '/mainq/market/market'
-      //icon: 'myfill'
+      image: cloudBase + "/imgs/人物角色/市场营销.png",
+      url: mainBase + '/mainq/market/market',
+      icon: 'myfill'
     },
     {
       title: '物流管理',
       name: '物流运送 提供公司物流保障',
       color: 'black',
-      url: mainBase + '/mainq/logistics/logistics'
-      //icon: 'icloading'
+      image: cloudBase + "/imgs/人物角色/物流管理.png",
+      url: mainBase + '/mainq/logistics/logistics',
+      icon: 'icloading'
     },
     ],
+  },
+  onLoad: function () { },
+  pageBack() {
+    wx.navigateBack({
+      delta: 1
+    });
+  },
+  cardSwiper(e) {
+    this.setData({
+      cardCur: e.detail.current
+    })
+  },
+  enterRole(e){
+    var cardCur = this.data.cardCur
+    var roles = this.data.elements
+    wx.redirectTo({
+      url: roles[cardCur].url,
+    })
   }
-})
+
+});
