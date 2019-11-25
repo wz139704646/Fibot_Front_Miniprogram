8e50c5cde86d42e5c579098fce890ff264cd5d2a
zjj <632291793@qq.com>
2019-11-25 13:58:27 +0800

    1125-zjj change the local searching in sellList to server searching

--- Fibot/miniprogram/pages/application/pages/sellList/sellList.js
+++ Fibot/miniprogram/pages/application/pages/sellList/sellList.js
@@ -12,27 +12,46 @@ Page({
     CustomBar: app.globalData.CustomBar,
     fun: null,
     sellList: [],
-    allList: [],
     page: 0,
-    allLoaded: false
+    allLoaded: false,
+    showSearchContents: false,
+    searchContents: [ 
+      "日期",
+      "客户姓名",
+      "销售单号"
+    ],
+    searchContentIdx: 0,
+    lastSearch: {
+      inputValue: "",
+      searchContentIdx: 0
+    }
   },
 
+  /**
+   * 延迟隐藏loading工具
+   * delay: 延迟的毫秒数
+   */
   delayHideLoading: function(delay) {
     setTimeout(() => {
       wx.hideLoading()
     }, delay)
   },
 
-  loadNewPage: function() {
+  /**
+   * 加载新页面
+   * param: 请求参数，在进行搜索或进行全部查询的时候有不同，需调用者传入
+   */
+  loadNewPage: function(param) {
+    if (!param) {
+      return
+    }
     let token = app.getToken()
     if (token) {
       let that = this
       let {page} = that.data
       page = page + 1
-      console.log('发起请求', {
-        companyId: app.globalData.companyId,
-        page
-      })
+      param['page'] = page
+      console.log('发起请求', param)
       wx.showLoading({
         title: '正在加载新数据',
         mask: true
@@ -44,12 +63,8 @@ Page({
           'Authorization': token
         },
         method: 'POST',
-        data: JSON.stringify({
-          companyId: app.globalData.companyId,
-          page
-        }),
+        data: JSON.stringify(param),
         success: res => {
-          // 添加拼音属性
           console.log(res)
           if (res.statusCode == 555) {
             app.relogin()
@@ -64,8 +79,8 @@ Page({
               title: res.data.errMsg || '请求失败', icon: 'none', duration: 1000
             })
           } else {
-            let list = res.data.result
-            if (list.length == 0) {
+            let newlist = res.data.result
+            if (newlist.length == 0) {
               console.log('返回空列表')
               wx.showToast({
                 title: '没有更多数据了',
@@ -77,62 +92,36 @@ Page({
               })
               return
             }
-            wx.cloud.callFunction({
-              name: 'convert2pinyin',
-              data: {
-                jsonStr: JSON.stringify(list),
-                options: {
-                  field: 'customerName',
-                  pinyin: 'pinyin'
-                }
-              }
-            }).then(res => {
-              // 存储索引列表和所有列表
-              console.log(res)
-              var newlist = res.result
-              var datelist = that.data.allList || []
-              var index1
-              for (let i in newlist) {
-                //newlist[i]['index'] = i
-                newlist[i]['date'] = newlist[i]['date'].toString().substring(0, 10)
-                newlist[i]['sum'] = that.calcTotal(newlist[i].goodsList)
-                index1 = that.ifDateInList(newlist[i].date, datelist)
-                //console.log(index1)
-                if (index1) {
-                  //console.log("add")
-                  datelist[index1 - 1].list.push(newlist[i])
-                } else {
-                  var list = []
-                  list.push(newlist[i])
-                  datelist.push({
-                    date: newlist[i].date,
-                    list: list
-                  })
-                }
-              }
-              // 后端已经做了排序
-              //datelist.sort(that.sortNumber)
-              console.log(datelist)
-              that.setData({
-                allList: datelist,
-                page,
-                allLoaded: false
-              }, () => {
-                // 加载完数据后自动做搜索
-                that.search({
-                  detail: {
-                    value: that.data.inputValue
-                  }
+            // 存储索引列表和所有列表
+            var datelist = that.data.sellList || []
+            var index1
+            for (let i in newlist) {
+              //newlist[i]['index'] = i
+              newlist[i]['date'] = newlist[i]['date'].toString().substring(0, 10)
+              newlist[i]['sum'] = that.calcTotal(newlist[i].goodsList)
+              index1 = that.ifDateInList(newlist[i].date, datelist)
+              //console.log(index1)
+              if (index1) {
+                //console.log("add")
+                datelist[index1 - 1].list.push(newlist[i])
+              } else {
+                var list = []
+                list.push(newlist[i])
+                datelist.push({
+                  date: newlist[i].date,
+                  list: list
                 })
-                that.delayHideLoading(500)
-              })
-            }).catch(err => {
-              wx.hideLoading()
-              console.error(err)
-              wx.showToast({
-                title: '商品信息出错',
-                icon: 'none'
-              })
+              }
+            }
+            // 后端已经做了排序
+            //datelist.sort(that.sortNumber)
+            console.log(datelist)
+            that.setData({
+              sellList: datelist,
+              page,
+              allLoaded: false
+            }, () => {
+              that.delayHideLoading(500)
             })
           }
         },
@@ -140,10 +129,62 @@ Page({
           wx.hideLoading()
           console.error(err1)
           wx.showToast({
-            title: '加载失败',
-            image: '../../../imgs/fail.png'
+            title: '请求失败',
+            icon: 'none',
+            duration: 2000
           })
         }
+      })
+    }
+  },
+
+  /**
+   * 进行搜索并加载新页
+   */
+  search: function (loadMore=false) {
+    let token = app.getToken()
+    if (token) {
+      var that = this
+      let { lastSearch, page, sellList } = that.data
+      let { inputValue, searchContentIdx } = lastSearch
+      if (!loadMore) {
+        // 如果是进行重新搜索，则重新获取搜索文本和搜索内容索引
+        inputValue = that.data.inputValue
+        searchContentIdx = that.data.searchContentIdx
+      }
+      let param = { companyId: app.globalData.companyId }
+      // 比较搜索内容是否变更过
+      if (!loadMore) {
+        // 搜索内容变动，将页数重置，列表清空
+        console.log('清空列表')
+        page = 0
+        sellList = []
+      }
+      that.setData({
+        page, sellList
+      }, () => {
+        // 设置完页面和销售列表后，进行新数据的加载
+        if (!inputValue) {
+          // 输入框没有信息
+          that.loadNewPage(param)
+          return
+        }
+        // 根据搜索内容，在请求参数中添加对应
+        switch (searchContentIdx) {
+        case 0:
+          // 进行日期搜索
+          param['date'] = inputValue
+          break
+        case 1:
+          // 进行客户姓名搜索
+          param['customerName'] = inputValue
+          break
+        case 2:
+          param['id'] = inputValue
+          break
+        }
+        // 进行新数据加载
+        that.loadNewPage(param)
       })
     }
   },
@@ -173,12 +214,12 @@ Page({
       this.setData({
         inputValue: searchValue
       }, () => {
-        // 等待搜索文本设置完毕后再加载新页面
-        this.loadNewPage()
+        // 等待搜索文本设置完毕后进行搜索
+        this.search()
       })
     } else {
-      // 加载新页面
-      this.loadNewPage()
+      // 进行搜索从而加载新页面
+      this.search()
     }
   },
 
@@ -187,10 +228,10 @@ Page({
    */
   onReachBottom: function() {
     console.log('触底加载')
-    this.loadNewPage()
     wx.pageScrollTo({
       scrollTop: 0,
     })
+    this.search(true)
   },
 
   //按日期排序
@@ -233,93 +274,52 @@ Page({
       url: '../recordInfo/recordInfo?back=sell' + '&id=' + id + '&fun=' + this.data.fun + '&status=' + status
     })
   },
-  //搜索
-  search: function (e) {
-    console.log(e)
-    var that = this
-    let searchText = e.detail.value
+
+  // 搜索内容文本被点击
+  onSearchContentSelect: function(e) {
+    console.log('选择搜索内容')
     this.setData({
-      inputValue: searchText
-    }, () => {
-      if (!searchText) {
-        this.setData({
-          sellList: this.data.allList
-        })
-        return
-      }
-      searchText = searchText.toLowerCase()
-      let slist = this.data.allList
-      let sellList = []
-      //查日期
-      for (var i in slist) {
-        if (slist[i].date.indexOf(searchText) != -1) {
-          sellList.push(slist[i])
-        }
-      }
-      //查姓名
-      var index1
-      for (var i in slist) {
-        for (var j in slist[i].list) {
-          var rlist = slist[i].list[j]
-          if (rlist.pinyin.indexOf(searchText) != -1 || rlist.customerName.indexOf(searchText) != -1) {
-            index1 = that.ifDateInList(rlist.date, sellList)
-            if (index1) {
-              //console.log("add")
-              sellList[index1 - 1].list.push(rlist)
-            } else {
-              var list = []
-              list.push(rlist)
-              sellList.push({
-                date: rlist.date,
-                list: list
-              })
-            }
-          }
-        }
-      }
-      //TODO查商品 还有bug 添加多次
-      for (var i in slist) {
-        for (var j in slist[i].list) {
-          var rlist = slist[i].list[j]
-          var ifbreak = false
-          for (var l in sellList) {
-            for (var m in sellList[l].list) {
-              //console.log(rlist.id + ",,," + sellList[l].list[m].id)
-              if (rlist.id == sellList[l].list[m].id) {
-                ifbreak = true
-              }
-              console.log(ifbreak)
-            }
-
-          }
-          if (ifbreak) {
-            break
-            console.log("break")
-          }
-          for (var k in slist[i].list[j].goodsList) {
-            // if (that.ifIdInList(rlist.id, sellList)){
-            //   break
-            // }
-            if (rlist.goodsList[k].goodsName.indexOf(searchText) != -1) {
-              index1 = that.ifDateInList(rlist.date, sellList)
-              if (index1) {
-                sellList[index1 - 1].list.push(rlist)
-              } else {
-                var list = []
-                list.push(rlist)
-                sellList.push({
-                  date: rlist.date,
-                  list: list
-                })
-              }
-            }
-          }
-        }
-      }
-
-      this.setData({
-        sellList: sellList
-      })
+      showSearchContents: true
     })
-  }
+  },
+
+  // 关闭搜索内容选择模态框
+  onModalClose: function (e) {
+    this.setData({
+      showSearchContents: false
+    })
+  },
+
+  // 确定搜索内容
+  onSearchContentConfirm: function(e) {
+    console.log('搜索内容更改为'+this.data.searchContents[e.detail.index])
+    this.setData({
+      showSearchContents: false,
+      searchContentIdx: e.detail.index
+    })
+  },
+
+  // 搜索框文本输入
+  onSearchTextInput: function(e) {
+    this.setData({
+      inputValue: e.detail.value
+    })
+  }, 
+
+  // 点击搜索按钮
+  onSearchButtonTapped: function(e) {
+    console.log('开始搜索')
+    let {lastSearch, inputValue, searchContentIdx} = this.data
+    // 检查点击时是否有必要进行新的搜索
+    if (lastSearch.inputValue != inputValue || (inputValue && lastSearch.searchContentIdx != searchContentIdx)) {
+      this.search()
+      // 将当前搜索的内容记为上次搜索的内容
+      this.setData({
+        lastSearch: {
+          inputValue: this.data.inputValue,
+          searchContentIdx: this.data.searchContentIdx
+        }
+      })
+    }
+  },
 })
